af0336f5c70f58928adce77e57179866f9391489
Konstantin <kmakarov@applicationcraft.com>
2025-11-14 17:32:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function delayedLog() {
-    await sleep(1000);
-    console.log("Simple 2 start")
-    await sleep(1000);
-    console.log("Simple 2 execute")
-    window.codioIDE.guides.addScript("https://pogostatus-martinarmani.codio.io/simple-script.js")
-    await sleep(1000);
-    console.log("Simple 2 done")
-}
-delayedLog();
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+// async function delayedLog() {
+//     await sleep(1000);
+//     console.log("Simple 2 start")
+//     await sleep(1000);
+//     console.log("Simple 2 execute")
+//     window.codioIDE.guides.addScript("https://pogostatus-martinarmani.codio.io/simple-script.js")
+//     await sleep(1000);
+//     console.log("Simple 2 done")
+// }
+// delayedLog();
+
+window.codioIDE.guides.addScript("https://pogostatus-martinarmani.codio.io/simple-script.js")
